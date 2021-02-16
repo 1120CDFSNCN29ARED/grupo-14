32ad2ee3e23c150bc969b17afa0978f270573491
mati.rosbaco@gmail.com <56400650+MatiasRosbaco@users.noreply.github.com>
2021-02-16 13:29:19 -0300

    funciona pero todo esta en /routes/index.js, creo que esto no tiene que funcionar asi pero no logro hacerlo de otra manera

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/producto', function(req, res) {
+    res.render('producto', { title: 'Express' })
+});
+
+router.get('/contact', function(req, res) {
+    res.render('contact', { title: 'Express' })
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { title: 'Express' })
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', { title: 'Express' })
 });
 
 module.exports = router;
