6c1d33a2e6fd80d71d031b934fcd6ffae57d92af
mati.rosbaco@gmail.com <56400650+MatiasRosbaco@users.noreply.github.com>
2021-02-28 17:33:20 -0300

    inicio sprint 4

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/producto', productRouter);
+//app.use('/producto', productRouter);
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,8 @@ router.get('/nuevoProducto', function(req, res) {
     res.render('nuevoProducto', { title: 'Express' })
 });
 
+router.get('/producto', function(req, res) {
+    res.render('producto', { title: 'Express' })
+});
+
 module.exports = router;
