842e7fbdd99c8c840879e78517f229152c4e6fc8
martinlias <martinliaskowsky@gmail.com>
2021-06-06 03:50:05 -0300

    agrego componente card

--- grupo-14/dashboard/src/components/ContentWrapper/ContentRowTop/ContentRowTop.jsx
+++ grupo-14/dashboard/src/components/ContentWrapper/ContentRowTop/ContentRowTop.jsx
@@ -1,6 +1,8 @@
 import React,{useState, useEffect, useRef} from 'react'
+import Card from '../../Card/Card';
 import BarriosInDb from './BarriosInDb/BarriosInDb'
 import TableContainer from './TableContainer/TableContainer'
+
 
 function ContentRowTop() {
     const [barrios, setBarriosList] = useState([]);
@@ -14,6 +16,7 @@ function ContentRowTop() {
         fetchBarrios();
     },[]);
     
+    
 
     return (
         <div>
@@ -26,56 +29,28 @@ function ContentRowTop() {
                 <div className="row">
 
                     {/* <!-- Movies in Data Base --> */}
-                    <div className="col-md-4 mb-4">
-                        <div className="card border-left-primary shadow h-100 py-2">
-                            <div className="card-body">
-                                <div className="row no-gutters align-items-center">
-                                    <div className="col mr-2">
-                                        <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
-                                        <div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
-                                    </div>
-                                    <div className="col-auto">
-                                        <i className="fas fa-film fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Card 
+                    title="Cantidad de propiedades"
+                    color="primary"
+                    value="100"
+                    icon="fa-building"
+                    />
 
                     {/* <!-- Total awards --> */}
-                    <div className="col-md-4 mb-4">
-                        <div className="card border-left-success shadow h-100 py-2">
-                            <div className="card-body">
-                                <div className="row no-gutters align-items-center">
-                                    <div className="col mr-2">
-                                        <div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-                                        <div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
-                                    </div>
-                                    <div className="col-auto">
-                                        <i className="fas fa-award fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Card
+                    title="Cantidad de agentes"
+                    color="success"
+                    value="20"
+                    icon="fa-male"
+                    />
 
                     {/* <!-- Actors quantity --> */}
-                    <div className="col-md-4 mb-4">
-                        <div className="card border-left-warning shadow h-100 py-2">
-                            <div className="card-body">
-                                <div className="row no-gutters align-items-center">
-                                    <div className="col mr-2">
-                                        <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
-											</div>
-                                        <div className="h5 mb-0 font-weight-bold text-gray-800">49</div>
-                                    </div>
-                                    <div className="col-auto">
-                                        <i className="fas fa-user fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Card
+                    title="Cantidad de usuarios"
+                    color="warning"
+                    value="300"
+                    icon="fa-user-circle"
+                    />
                 </div>
                 {/* <!-- End movies in Data Base --> */}
 
